43b1692d7c20612c51fa4cbc51937cceff97898c
gaurav <dggs222@gmail.com>
2017-03-19 20:39:41 +0530

    Deploye commit

--- Inventory/settings.py
+++ Inventory/settings.py
@@ -132,5 +132,6 @@ STATICFILES_DIRS = (
 )
 if not DEBUG:
     DATABASES['default'] =  dj_database_url.config()
-
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
