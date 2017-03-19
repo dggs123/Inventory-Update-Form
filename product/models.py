9fc40d249e7e5d146fa23aa95a1a572d89ca86af
gaurav <dggs222@gmail.com>
2017-03-19 21:22:07 +0530

    Deploye commit

--- product/models.py
+++ product/models.py
@@ -15,4 +15,4 @@ class Product(models.Model):
     price = models.IntegerField(help_text='Numeric, Eg: 50000')
     quantity = models.IntegerField(help_text='Numeric, Eg: 35')
     product_type = models.CharField(max_length=20, choices=PRODUCT_TYPE, default='tv', blank=True, null=True)
-    cover = models.URLField(blank=True, default='', help_text='image link for cover (1300x500)')
+    cover = models.URLField(blank=True, default='', help_text='image link for cover')
