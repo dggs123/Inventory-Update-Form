042dcb7c71747e55c8d6e7d395c7d79e79e02cfe
gaurav <dggs222@gmail.com>
2017-04-09 12:55:06 +0530

    new code

--- Inventory/settings.py
+++ Inventory/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-if DEBUG:
+if not DEBUG:
     DATABASES['default'] =  dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
